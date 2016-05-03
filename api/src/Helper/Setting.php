25a8878da5d0812db059afd59421ed4001c2a960
hprasetyou <hprasetyou@gmail.com>
2016-05-03 13:11:51 +0700

    test

--- api/src/Helper/Setting.php
+++ api/src/Helper/Setting.php
@@ -13,7 +13,7 @@ class Setting{
                 "username"=>"hprasetyou",
                 "password"=>"Rp.15000", //Rp.15000
                 "dbname"=>"KSAAS_main",
-                "dbhost"=>"localhost" //127.3.136.2
+  //              "dbhost"=>"localhost" //127.3.136.2
                "dbhost"=>"127.3.136.2" //
              )
          );
