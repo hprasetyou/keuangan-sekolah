e084203a317be23e0f0457914cbfc89cb3d16c84
hprasetyou <hprasetyou@gmail.com>
2016-05-03 12:51:15 +0700

    test

--- api/src/Helper/Setting.php
+++ api/src/Helper/Setting.php
@@ -7,14 +7,14 @@ class Setting{
   public static function get()
      {
          return array(
-             "path"=>"web/keuangansekolah/api/index.php",
-//             "path"=>"api/index.php",
+//             "path"=>"web/keuangansekolah/api/index.php",
+             "path"=>"api/index.php",
              "db"=>array(
                 "username"=>"hprasetyou",
                 "password"=>"Rp.15000", //Rp.15000
                 "dbname"=>"KSAAS_main",
                 "dbhost"=>"localhost" //127.3.136.2
-//               "dbhost"=>"127.3.136.2" //
+               "dbhost"=>"127.3.136.2" //
              )
          );
      }
