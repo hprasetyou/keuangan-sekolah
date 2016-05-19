61fadd0191cf4d80cac61980452ee8593e145f3a
hprasetyou <hprasetyou@gmail.com>
2016-05-19 11:15:21 +0700

    fixbug rencana anggaran attemp 1

--- js/dashboard/controller.js
+++ js/dashboard/controller.js
@@ -121,7 +121,7 @@ function($scope,tapelService,ra,jenis_transaksi,$rootScope,akun,helper){
     $scope.adatapel=[]
     for(var i=0; i<$scope.lsttapel.length; i++){
       ra.Tahun({tahun:$scope.lsttapel[i]}).then(function(response){
-        $scope.adatapel[i]=response.length*1
+        
         console.log($scope.adatapel[i] + ' / '+ $scope.lsttapel.length);
       })
       $scope.nmtapel[i]="20"+$scope.lsttapel[i].substr(0,2)+"/20"+$scope.lsttapel[i].substr(2,2)
