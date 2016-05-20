87bc25cbb81674048ac9285d525c87922870811c
hprasetyou <hprasetyou@gmail.com>
2016-05-20 10:27:48 +0700

    user level distribution

--- js/landing/app.js
+++ js/landing/app.js
@@ -79,7 +79,6 @@ app.config(['$routeProvider','$locationProvider',function($routeProvider,$locati
   app.controller('daftar',['$scope','$http','$location','User',
   function($scope,$http,$location,User){
 		$scope.form_register={}
-		$scope.form_register.user_level='2';
 
 					$scope.daftar = function(){
 
