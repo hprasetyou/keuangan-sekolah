795ce7877fe66a0a93e8b03c7a03ca71c841c960
hprasetyou <hprasetyou@gmail.com>
2016-05-22 19:04:00 +0700

    fix user_level and jurnal datatype

--- api/src/Helper/Setting.php
+++ api/src/Helper/Setting.php
@@ -7,16 +7,16 @@ class Setting{
   public static function get()
      {
          return array(
-            "root"=>"http://localhost/web/keuangansekolah",
-      //      "root"=>"http://keuangansekolah-hprasetyou.rhcloud.com",
-             "path"=>"web/keuangansekolah/api/index.php",
-      //     "path"=>"api/index.php",
+      //      "root"=>"http://localhost/web/keuangansekolah",
+            "root"=>"http://keuangansekolah-hprasetyou.rhcloud.com",
+  //           "path"=>"web/keuangansekolah/api/index.php",
+           "path"=>"api/index.php",
              "db"=>array(
                 "username"=>"hprasetyou",
                 "password"=>"Rp.15000", //Rp.15000
                 "dbname"=>"KSAAS_main",
-               "dbhost"=>"localhost"
-    //           "dbhost"=>"127.3.136.2" //
+    //           "dbhost"=>"localhost"
+               "dbhost"=>"127.3.136.2" //
              )
          );
      }
