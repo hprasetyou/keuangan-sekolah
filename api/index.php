c8434b579ddc9de2a98560bdc8f5391250b23206
hprasetyou <hprasetyou@gmail.com>
2016-05-31 19:54:13 +0700

    fix rencana_anggaran

--- api/index.php
+++ api/index.php
@@ -1,6 +1,6 @@
 <?php
-error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE); ini_set('display_errors','On');
-//error_reporting(0);
+//error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE); ini_set('display_errors','On');
+error_reporting(0);
 require 'vendor/autoload.php';
 
 use Phroute\Phroute\RouteCollector;
